d06f1c3a082c3da2cdd41a82e7d44021ead8662a
Maria Ines <133809034+MariaInesPdL@users.noreply.github.com>
2023-06-20 14:58:33 -0400

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -1,3 +1,4 @@
+//ADDING SOME COMMENTS TO THE USERPROFILE.JAVA CLASS
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
